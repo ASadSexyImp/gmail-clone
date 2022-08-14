a20946e48f1216039f21a438043e900c2bffff14
ASadSexyImp <masakazuplus@gmail.com>
2022-08-14 18:08:43 +0900

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
+import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Sidebar />
+      </div>
+    </Router>
   );
 }
 
