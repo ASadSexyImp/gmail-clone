99a731967288ec41d7508cd6915f7c01f87608c2
ASadSexyImp <masakazuplus@gmail.com>
2022-08-14 17:49:53 +0900

    header

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import "./Header.css"
+import MenuIcon from '@material-ui/icons/Menu'
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header_left">
+                <IconButton>
+                    <MenuIcon />
+                </IconButton>
+                <img
+                    src="https://the8-bit.com/wp-content/uploads/2020/10/Redesigned-Gmail.jpg"
+                    alt=""
+                />
+            </div>
+            <div className="header_middle">
+                <SearchIcon />
+                <input placeholder="Search mail" type="text" />
+                <ArrowDropDownIcon className="header_inputCart" />
+            </div>
+            <div className="header_right">
+                <IconButton>
+                    <AppsIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsIcon />
+                </IconButton>
+                <Avatar />
+            </div>
+        </div>
+    )
+}
+
+export default Header
